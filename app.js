e8044164ebfadaf4f0028398e4ec98fc4617e829
Error3021 <deante.agleham@gmail.com>
2022-10-13 15:27:00 -1000

    The variables

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //functionality for the birds, points, etc.
-
+var score;
+var shots;
+var birb = document.createElement("button");
+var wave;
+var startRetry = document.createElement("button");
 
 //moving the birds
 
