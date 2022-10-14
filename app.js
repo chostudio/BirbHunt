dec0541fc5253bf221213eac2d3f4de7b5a1854c
Minecraft-Cake <96643025+Minecraft-Cake@users.noreply.github.com>
2022-10-13 15:23:30 -1000

    Gotta change some html so posting here just to be safe Added gamestate enum

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-//functionality for the birds, points, etc.
+class GameState {
+    static MENU = new GameState('menu')
+    static PLAYING = new GameState('playing')
+    static GAME_OVER = new GameState('game_over')
 
+    constructor(name) {
+        this.name = name
+    }
+    toString() {
+        return `GameState.${this.name}`
+    }
+}
+
+var gameState = GameState.MENU
+
+
+
+
+
+
+//functionality for the birds, points, etc.
 
 //moving the birds
 
-
-
-
 //cursor on click
-
-
-
 
 //points
